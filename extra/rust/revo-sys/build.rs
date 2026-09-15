c439ddb540544f48ff1769c5a91522779c9f84ce
doomy <codeberg.org.matrimony703@passmail.net>
2026-09-14 22:21:40 -0400

    rust ffi

---
+++ extra/rust/revo-sys/build.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("../../../zig-out/include/revo/revo.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .raw_line("unsafe impl Sync for RevoBinding {}")
+        .allowlist_item(".?(?i-u:revo).*")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ extra/rust/revo-sys/examples/basic.rs
@@ -0,0 +1,39 @@
+//! # Use this example
+//!
+//! first build with
+//!
+//! ```
+//! cargo build -p revo-sys --example basic
+//! ```
+//!
+//! then open `revo` and use the function to return 0
+//!
+//! ```rb
+//! import "./target/debug/examples/libbasic.dylib".hi()
+//! > 0
+//! ```
+
+use std::ffi::*;
+
+use revo_sys::*;
+
+// unsafe impl Sync for Binding {}
+
+extern "C" fn hi(_vm: *mut c_void, _argc: usize, _argv: *mut RevoData, out: *mut RevoData) {
+    unsafe {
+        *out = 0;
+    }
+}
+// the array must end with a NULL/NULL terminator row
+#[unsafe(no_mangle)]
+pub static revo_bindings: [RevoBinding; 2] = [
+    RevoBinding {
+        name: c"hi".as_ptr(),
+        fn_: Some(hi),
+    },
+    // null termination defines the end of a list of bindings
+    RevoBinding {
+        name: std::ptr::null(),
+        fn_: None,
+    },
+];
